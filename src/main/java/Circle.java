4a56b296324744c5980e7e2f3b4d485bf66492c8
Thomas Pendleton III <105595323+Tpenny68@users.noreply.github.com>
2025-04-02 22:58:13 -0400

    Submission

--- src/main/java/Circle.java
+++ src/main/java/Circle.java
@@ -1,124 +1,49 @@
-/**
- * This class provides a simple Circle type that
- * creates and tracks instances of circles.
- *
- * @author (You & Me)
- * @version (2019-10-15)
- */
-public class Circle
-{
-    //instance variables
+public class Circle {
     private Point location;
     private double radius;
 
-    /**
-     * Default constructor that initializes an instance of Circle
-     * to (0,0) with a radius of 1.0.
-     */
-    public Circle()
-    {
-        //TODO: replace this line with your code
+    public Circle() {
+        this.location = new Point(0, 0);
+        this.radius = 1.0;
     }
 
-    /**
-     * Constructor that initializes an instance of Circle to (0,0)
-     * with a radius given by the parameter radius.
-     *
-     * @param radius the radius of this circle.
-     */
-    public Circle(double radius)
-    {
-        //TODO: replace this line with your code
+    public Circle(double radius) {
+        this.location = new Point(0, 0);
+        this.radius = radius;
     }
 
-    /**
-     * Constructor that initializes an instance of Circle to (x,y)
-     * with a radius given by the parameter radius.
-     *
-     * @param location the location of the center of this circle
-     * @param radius the radius of this circle.
-     */
-    public Circle(Point location, double radius)
-    {
-        //TODO: replace this line with your code
+    public Circle(Point location, double radius) {
+        this.location = new Point(location);
+        this.radius = radius;
     }
 
-    //accessors
-    /**
-     * Returns the radius of this circle.
-     *
-     * @return The radius of this circle.
-     */
-    public double getRadius()
-    {
-        //TODO: replace this line and the line below with your code
-        return 0.0;
+    public double getRadius() {
+        return radius;
     }
 
-    /**
-     * Returns an instance of a Point indicating the location of this circle.
-     *
-     * @return The location of this circle.
-     */
-    public Point getLocation()
-    {
-        //TODO: replace this line and the line below with your code
-        return null;
+    public Point getLocation() {
+        return new Point(location);
     }
 
-    // mutator methods
-    /**
-     * Sets the radius of this circle.
-     *
-     * @param radius The new radius of this circle.
-     */
-    public void setRadius(double radius)
-    {
-        //TODO: replace this line with your code
+    public void setRadius(double radius) {
+        this.radius = radius;
     }
 
-    /**
-     * Sets the location of this circle.
-     *
-     * @param location The new location this circle.
-     */
-    public void setLocation(Point location)
-    {
-        //TODO: replace this line with your code
+    public void setLocation(Point location) {
+        this.location = new Point(location);
     }
 
-    // other methods
-    /**
-     * Returns true if the point is in the circle; otherwise false. A point
-     * falling on the circle is considered inside the circle.
-     *
-     * @return true if the point is in the circle; otherwise false
-     */
-    public boolean contains(Point p)
-    {
-        //TODO: replace this line and the line below with your code
-        return false;
+    public boolean contains(Point p) {
+        return location.distanceTo(p) <= radius;
     }
 
-    /**
-     * Returns true if the Circle aCircle intersects with this circle;
-     * otherwise false.
-     *
-     * @param aCircle a circle.
-     */
-    public boolean intersects(Circle aCircle)
-    {
-        //TODO: replace this line and the line below with your code
-        return false;
+    public boolean intersects(Circle aCircle) {
+        double distance = this.location.distanceTo(aCircle.getLocation());
+        return distance <= (this.radius + aCircle.getRadius());
     }
 
-    /**
-     * Returns a string representation of this circle.
-     *
-     * @return a string representation of this circle.
-     */
-    public String toString()
-    {
-        return "Circle/loc="+location+",radius="+radius;
+    @Override
+    public String toString() {
+        return "Circle/loc=" + location + ",radius=" + radius;
     }
 }
